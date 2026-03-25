5c528e8c9768448811653f760940ecc59a31c509
Sohamtam46 <sohamtam46@gmail.com>
2026-03-25 15:27:53 +0000

    removed redundunt code

--- Day 23 - Turtle Crossing Game/main.py
+++ Day 23 - Turtle Crossing Game/main.py
@@ -22,19 +22,22 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+
+    # create and move the cars
     car_manager.create_cars()
     car_manager.car_move()
 
+    # detect player reaching finish line and level up
     if player.ycor() >= 300:
         player.position_reset()
         scoreboard.level_increase()
         car_manager.level_increase()
 
+    # detect collision of player with car
     for car in car_manager.cars:
         if player.distance(car) < 30:
             game_is_on = False
             car_manager.game_over()
             scoreboard.game_over()
-
 
 screen.exitonclick()
