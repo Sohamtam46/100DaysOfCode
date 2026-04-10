aba4a6af9340c585ddad5194d8a6afb1d020ce14
Sohamtam46 <sohamtam46@gmail.com>
2026-04-10 19:48:23 +0100

    added functionality to generate random password as well as save login info

--- Day 29/main.py
+++ Day 29/main.py
@@ -1,50 +1,88 @@
 from tkinter import *
+from tkinter import messagebox
+import data
+import random
+import pyperclip
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+def generate_password():
+    password_input.delete(0, END)
+    password_list = []
+
+    password_list += [random.choice(data.letters) for _ in range(2)]
+    password_list += [random.choice(data.numbers) for _ in range(2)]
+    password_list += [random.choice(data.symbols) for _ in range(2)]
+
+    random.shuffle(password_list)
+    password = ''.join(password_list)
+    password_input.insert(0, password)
+    pyperclip.copy(password)
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def add_login_data():
+
+    website = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
+
+    # validation for input fields
+    if len(website) == 0:
+        messagebox.showerror(title="Missing Data!", message="You haven't entered the website address! Please add.")
+        website_input.focus()
+    elif len(email) == 0:
+        messagebox.showerror(title="Missing Data!", message="You haven't entered the email address! Please add.")
+        email_input.focus()
+    elif len(password) == 0:
+        messagebox.showerror(title="Missing Data!",
+                             message="You haven't entered the Password! Either generate or add one.")
+        password_input.focus()
+    else:
+        messagebox.showinfo(title="Saved!", message="Your Login Details have been Saved!")
+        with open("login.txt", mode="a") as data:
+            data.write(f"\n{website} | {email} | {password}")
+
+        # helps clear the inputs after saving the login data
+        website_input.delete(0, END)
+        password_input.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------ #
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=100,pady=50)
+window.config(padx=50, pady=50)
 
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=1, row=0)
 
-canvas = Canvas(width=200,height=192)
-logo_img =PhotoImage(file="logo.png")
-canvas.create_image(100,112,image = logo_img)
-canvas.grid(column=1,row=0)
+website_label = Label(text="Website")
+website_label.grid(column=0, row=1)
 
-website_label =Label(text="Website")
-website_label.grid(column=0,row=1)
+website_input = Entry(width=52)
+website_input.grid(column=1, row=1, columnspan=2, sticky=W)
+website_input.focus()
 
-website_input = Entry(width=50)
-website_input.grid(column=1,row=1)
+email_label = Label(text="Email/Username")
+email_label.grid(column=0, row=2)
 
-email_label =Label(text="Email/Username")
-email_label.grid(column=0,row=2)
+email_input = Entry(width=52)
+email_input.insert(0, "soham@gmail.com")
+email_input.grid(column=1, row=2, columnspan=2, sticky=W)
 
-email_input = Entry(width=50)
-email_input.grid(column=1,row=2)
+password_label = Label(text="Password")
+password_label.grid(column=0, row=3)
 
-password_label =Label(text="password")
-password_label.grid(column=0,row=3)
+password_input = Entry(width=30)
+password_input.grid(column=1, row=3, sticky=W)
 
-password_input = Entry(width=25)
-password_input.grid(column=1,row=3,)
+generate_pass_button = Button(text="Generate Password", command=generate_password)
+generate_pass_button.grid(column=2, row=3)
 
-generate_pass_button = Button(text="Generate Password")
-generate_pass_button.grid(column=2,row=3)
-
-calculate_button = Button(text="Add",width=42)
-calculate_button.grid(column=1,row=4)
-
-
-
-
-
-
-
-
+add_button = Button(text="Add", width=45, command=add_login_data)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
