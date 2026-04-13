3add0ded26f27b538ffaae65781ab83665ff3e7d
Sohamtam46 <sohamtam46@gmail.com>
2026-04-13 07:08:49 +0100

    improved phonetic project with try-except block

--- Day 30/Day 30 - NATO Phonetic Alphabet Project Addition/main.py
+++ Day 30/Day 30 - NATO Phonetic Alphabet Project Addition/main.py
@@ -4,7 +4,16 @@ nato_data = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_data_df = pd.DataFrame(nato_data)
 nato_data_dict = {row.letter:row.code for (index,row) in nato_data_df.iterrows()}
 
+app_in_use = True
 
-user_word = input("Enter the word you need the phonetic code words for:\n").upper()
-phonetic_code_word = {letter:nato_data_dict[letter] for letter in user_word}
-print(phonetic_code_word)
+def generate_phonetic():
+    try:
+        user_word = input("Enter the word you need the phonetic code words for:\n").upper()
+        phonetic_code_word = {letter:nato_data_dict[letter] for letter in user_word}
+    except KeyError:
+        print("Please Enter letters only.")
+        generate_phonetic()
+    else:
+        print(phonetic_code_word)
+
+generate_phonetic()
