b5930147617fbf27b94b1cd70dbcf80588e36710
Sohamtam46 <sohamtam46@gmail.com>
2026-04-16 11:21:59 +0100

    Adding Tkinter modules to Quizz app

--- Day 34 - Quiz App/main.py
+++ Day 34 - Quiz App/main.py
@@ -1,6 +1,9 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
+
+
 
 question_bank = []
 for question in question_data:
@@ -9,7 +12,7 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-
+quizz_ui = QuizInterface()
 quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
--- Day 34 - Quiz App/ui.py
+++ Day 34 - Quiz App/ui.py
@@ -1,2 +1,37 @@
+from tkinter import *
+
+
 THEME_COLOR = "#375362"
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20,pady=20,bg=THEME_COLOR)
+
+        self.score_Label = Label(text=f"Score : 0", bg=THEME_COLOR, fg="White")
+        self.score_Label.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=300, height=250, bg="White")
+        self.q_text = self.canvas.create_text(150, 125, text=f"Question", fill=THEME_COLOR, font=("Ariel", 20, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        true_button_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_button_img, highlightthickness=0)
+        self.true_button.grid(column=0, row=2)
+
+        false_button_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_button_img, highlightthickness=0)
+        self.false_button.grid(column=1, row=2)
+
+        self.window.mainloop()
+
+
+
+
+
+
+
+
 
