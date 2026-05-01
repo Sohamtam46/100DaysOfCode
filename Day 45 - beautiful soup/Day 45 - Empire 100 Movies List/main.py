27c73ccb0c19b4e61efb63dbd706236112dd4caf
Sohamtam46 <sohamtam46@gmail.com>
2026-05-01 12:40:57 +0100

    Day 45 final project

---
+++ Day 45 - beautiful soup/Day 45 - Empire 100 Movies List/main.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+# URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+
+# Write your code below this line 👇
+
+response = requests.get(url=URL)
+site_html = response.text
+soup = BeautifulSoup(site_html,"html.parser")
+all_movie_titles = soup.select(selector=".article-title-description__text > h3")
+
+title = []
+
+for movie_title in all_movie_titles:
+    title.append(movie_title.getText())
+
+for movie in reversed(title):
+    with open("Best Movies.txt","a", encoding="utf-8") as file:
+        file.write(movie + "\n")
